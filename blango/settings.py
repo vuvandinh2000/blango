c7b76b46a65787add4ea492ae2729d6e39c1b368
Vu Van Dinh <dinhvu@synopsys.com>
2025-12-28 20:12:35 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -141,6 +141,41 @@ class Dev(Configuration):
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+    ADMINS = [("Dinh Vu", "vuvandinh2000@gmail.com")
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler", 
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+            "mail_admins": {
+                "level": "ERROR",
+                "class": "django.utils.log.AdminEmailHandler",
+                "filters": ["require_debug_false"],
+            },
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        }
+    }
 
 class Prod(Dev):
     DEBUG = False
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,22 @@
+import logging
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 
+logger = logging.getLogger(__name__)
+
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug("Got %d posts", len(posts))
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    logger.info(
+        "Created comment on Post %d for user %s", post.pk, request.user
+    )
     if request.user.is_active:
         if request.method == "POST":
             comment_form = CommentForm(request.POST)
