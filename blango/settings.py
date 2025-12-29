d75ff1b9d2a9881f6c7fdd35e7361af762afe090
Vu Van Dinh <dinhvu@synopsys.com>
2025-12-29 11:38:48 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -50,6 +50,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        "blango_auth",
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
@@ -191,6 +192,8 @@ class Dev(Configuration):
         'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
     INTERNAL_IPS = ["192.168.10.31"]
+
+    AUTH_USER_MODEL = "blango_auth.User"
 
 class Prod(Dev):
     DEBUG = False
