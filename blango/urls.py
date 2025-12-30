d341e18de22bf6a4362741215db3d99466636bef
Vu Van Dinh <dinhvu@synopsys.com>
2025-12-30 09:31:04 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("allauth.urls")),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
