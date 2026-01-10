364de2a06c35962dfdb9fc4a68d71d4bf554af38
Vu Van Dinh <dinhvu@synopsys.com>
2026-01-10 10:11:36 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -61,7 +61,16 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
     ]
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
     MIDDLEWARE = [
         "debug_toolbar.middleware.DebugToolbarMiddleware",
